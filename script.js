9e19592052ace3b8e5e3a8be75557da92122e2c7
sohelhussain <sohelhussaing@gmail.com>
2024-01-31 22:55:20 -0800

    feat:-moving text in container-five

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-const locoScrolling = () => {
-  gsap.registerPlugin(ScrollTrigger);
+// const locoScrolling = () => {
+//   gsap.registerPlugin(ScrollTrigger);
 
-  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+//   // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
-  const locoScroll = new LocomotiveScroll({
-    el: document.querySelector("#main"),
-    smooth: true,
-  });
-  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-  locoScroll.on("scroll", ScrollTrigger.update);
+//   const locoScroll = new LocomotiveScroll({
+//     el: document.querySelector("#main"),
+//     smooth: true,
+//   });
+//   // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+//   locoScroll.on("scroll", ScrollTrigger.update);
 
-  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-  ScrollTrigger.scrollerProxy("#main", {
-    scrollTop(value) {
-      return arguments.length
-        ? locoScroll.scrollTo(value, 0, 0)
-        : locoScroll.scroll.instance.scroll.y;
-    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-    getBoundingClientRect() {
-      return {
-        top: 0,
-        left: 0,
-        width: window.innerWidth,
-        height: window.innerHeight,
-      };
-    },
-    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-    pinType: document.querySelector("#main").style.transform
-      ? "transform"
-      : "fixed",
-  });
+//   // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+//   ScrollTrigger.scrollerProxy("#main", {
+//     scrollTop(value) {
+//       return arguments.length
+//         ? locoScroll.scrollTo(value, 0, 0)
+//         : locoScroll.scroll.instance.scroll.y;
+//     }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+//     getBoundingClientRect() {
+//       return {
+//         top: 0,
+//         left: 0,
+//         width: window.innerWidth,
+//         height: window.innerHeight,
+//       };
+//     },
+//     // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+//     pinType: document.querySelector("#main").style.transform
+//       ? "transform"
+//       : "fixed",
+//   });
 
-  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
-  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+//   // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+//   ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-  ScrollTrigger.refresh();
-};
-locoScrolling();
+//   // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+//   ScrollTrigger.refresh();
+// };
+// locoScrolling();
 
 const loderEffect = () => {
   let tl = gsap.timeline();
@@ -181,3 +181,29 @@ const sherryEffect = () => {
   });
 };
 sherryEffect();
+
+// bug:- scrollTrigger are not working properly but gsap are working
+gsap.to(".hed-mar-left",{
+  x:-1000,
+  duration: 10,
+  scrollTrigger:{
+    trigger: "#container-five",
+    scroller:"#main",
+    // start:"top 150%",
+    // end:"bottom -50%",
+    // scrub: true,
+    marker: true
+  }
+})
+gsap.from(".hed-mar-right",{
+  x:-1000,
+  duration: 10,
+  scrollTrigger:{
+    trigger: "#container-five",
+    scroller:"#main",
+    // start:"top 150%",
+    // end:"bottom -50%",
+    // scrub: true,
+    marker: true
+  }
+})
